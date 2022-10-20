e866a1ba67f6544a5c979c1f75aa8e1b52a07d2a
QuagliataAlberto <113895221+QuagliataAlberto@users.noreply.github.com>
2022-10-20 11:24:21 +0200

    Create PaginaRiservata.php

---
+++ PHP/B_ES02/PaginaRiservata.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>ITCS Erasmo da Rotterdam</title>
+</head>
+<body>
+	<h1>Richiesta credenziali per l'accesso all'area riservata</h1><br/>
+	
+	<form name="frmLogin" action="login.php" method="POST">
+		User name: <input type="text" name="username"><br>
+		Password: <input type="password" name="password"><br>
+		<input type="submit" value="Accedi">
+	</form>
+
+</body>
+</html>
