8f4d33413d334a043e983a7e826855f1e355aa1b
QuagliataAlberto <113895221+QuagliataAlberto@users.noreply.github.com>
2022-11-11 19:38:35 +0100

    Update index.php

---
+++ PHP/B_ES02 - Accesso a Pagina Riservata - b/index.php
@@ -0,0 +1,40 @@
+<!-- Alberto Angelo Quagliata -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login pagina</title>
+</head>
+
+<body>
+<h2>Inserisci le credenziali</h2>
+<?php
+
+if(isset($_POST['username']) && ($_POST['password']))
+{
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+		if($username!="admin" || $password!="password") 
+		{
+			echo "<h2>Credenziali sbagliate! :(</h2>";
+		} 
+		
+		
+		else 
+		{
+			echo "<h2>Benvenuto  :) " . $username . " nell'area riservata!</h2>";
+		}
+
+}else {
+
+	<form name="frmLogin" action="index.php" method="POST">
+	Username: <input type="text" name="username"><br>
+	Password: <input type="password" name="password"><br>
+	<input type="submit" value="Submit">
+</form>
+
+
+}
+
+
+</body>
+</html>
