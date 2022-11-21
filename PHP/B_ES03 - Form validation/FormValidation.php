e9d7af836143fe4700b31b3e5a42c7eb59ac8fc4
QuagliataAlberto <113895221+QuagliataAlberto@users.noreply.github.com>
2022-11-22 00:04:17 +0100

    Create FormValidation.php

---
+++ PHP/B_ES03 - Form validation/FormValidation.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>ITCS Erasmo da Rotterdam</title>
+</head>
+<body>
+	<h2>Validation Form</h2>
+	<h1>compilare obbligatoriamente i seguenti campi</h1>
+	<form name="frmLogin" action="Check.php" method="POST">
+		
+		 - Nome: <input type="text" name="nome"><br>
+		 - Cognome: <input type="text" name="cognome"><br>
+		 - Data di nascita: <input type="date" name="data"><br>
+		 - Nickname: <input type="text" name="nickname"><br>
+		 - Email: <input type="text" name="email"><br>
+		 - Cellulare: <input type="text" name="cellulare"><br>
+		 - Password: <input type="password" name="password"><br>
+			<input type="submit" value="Submit">
+		
+	</form><hr/>
+
+</body>
+</html>
