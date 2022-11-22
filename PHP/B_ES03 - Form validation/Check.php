c973ca84626f4477499241850df9a6c54d754123
QuagliataAlberto <113895221+QuagliataAlberto@users.noreply.github.com>
2022-11-22 13:31:58 +0100

    Update Check.php

--- PHP/B_ES03 - Form validation/Check.php
+++ PHP/B_ES03 - Form validation/Check.php
@@ -1,19 +1,31 @@
 <!-- Alberto Angelo Quagliata -->
 <?php
 
-$nome=trim($_POST["nome"]);  /*assegno al campo nome il valore nome*/
-$cognome=trim($_POST["cognome"]);
-$data=trim($_POST["data"]);
-$nickname=trim($_POST["nickname"]);
-$email=trim($_POST["email"]);
-$cellulare=trim($_POST["cellulare"]);
-$password=trim($_POST["password"]);
+/*  inizializzazione delle variabili  */
+
+$nome = $cognome = $data = $nickname = $email = $cellulare = $password = "";
+
+//if(isset($_POST["submit"]))
+{
+	
+	$nome=trim($_POST["nome"]);  /*assegno al campo nome il valore nome*/
+	$cognome=trim($_POST["cognome"]);
+	$data=trim($_POST["data"]);
+	$nickname=trim($_POST["nickname"]);
+	$email=trim($_POST["email"]);
+	$cellulare=trim($_POST["cellulare"]);
+	$password=trim($_POST["password"]);
+	
+}
+	
+
 
 
 if(preg_match('/^[a-zA-Z]*$/',$nome))
 	{
 		echo "Nome inserito: ";
 		echo $nome;
+		echo "<br>";
 	}
 
 else
@@ -29,6 +41,7 @@ if(preg_match('/^[a-zA-Z]*$/',$cognome))
 	{
 		echo "<br>Cognome inserito: ";
 		echo $cognome;
+		echo "<br>";
 	}	
 	
 else
@@ -43,6 +56,7 @@ if(preg_match('/^[a-zA-Z0-9]*$/',$nickname))
 	{
 		echo "<br>Nickname inserito: ";
 		echo $nickname;
+		echo "<br>";
 	}	
 	
 else
@@ -58,6 +72,7 @@ if(preg_match('/^[a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/',$email))
 	{
 		echo "<br>Email inserita: ";
 		echo $email;
+		echo "<br>";
 	}
 	
 else
@@ -69,6 +84,7 @@ if(preg_match('/^[0-9]/', $cellulare))
 	{
 		echo "<br>Numero di cellulare inserito: ";
 		echo $cellulare;
+		echo "<br>";
 	}
 	
 else
@@ -80,6 +96,7 @@ if(preg_match('/^[a-zA-Z_0-9]/', $password))
 	{
 		echo "<br>password inserita: ";
 		echo $password;
+		echo "<br>";
 	}
 	
 ?>
