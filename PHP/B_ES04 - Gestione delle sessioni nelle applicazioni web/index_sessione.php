8e0e13ba96ca25700ace1539c885fff6d3a4f12b
QuagliataAlberto <113895221+QuagliataAlberto@users.noreply.github.com>
2022-12-22 11:37:41 +0100

    Update and rename index.php to index_sessione.php

---
+++ PHP/B_ES04 - Gestione delle sessioni nelle applicazioni web/index_sessione.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+		<title>Home page</title>
+</head>
+<body>
+<h1>Home page</h1><hr/>
+<a href="Login.php"><h2>Esegui Login</a><br/></h2>
+<a href="PaginaRis.php"><h2>Vai alla pagina riservata</a><br/></h2>
+<a href="Logout.php"><h2>Esegui logout</a><br/></h2>
+
+<hr/>
+</body>
+</html>
