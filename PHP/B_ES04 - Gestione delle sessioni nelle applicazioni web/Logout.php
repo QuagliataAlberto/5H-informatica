8ee82434b6c36f2d6bcd9a7c3b400bc9af88bb48
QuagliataAlberto <113895221+QuagliataAlberto@users.noreply.github.com>
2022-12-22 11:39:23 +0100

    Create Logout.php

---
+++ PHP/B_ES04 - Gestione delle sessioni nelle applicazioni web/Logout.php
@@ -0,0 +1,22 @@
+<!-- Alberto Angelo Quagliata -->
+<html>
+<head>
+	<title>Uscita - ITCS Erasmo da Rotterdam</title>
+</head>
+
+<body>
+
+<?php
+
+	session_destroy();
+	echo"Logout avvenuto, Arrivederci";
+
+
+?>
+<hr>
+
+	<a href="Login.php"><h2>Accedi di nuovo</h2></a>
+	<a href="index_session.php"><h2>Torna alla Home</h2></a>
+
+</body>
+</html>
