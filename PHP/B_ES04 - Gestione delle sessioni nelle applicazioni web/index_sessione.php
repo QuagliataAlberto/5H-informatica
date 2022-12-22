60659450ef5bf2263cdbb5d47696e55d284d0087
QuagliataAlberto <113895221+QuagliataAlberto@users.noreply.github.com>
2022-12-22 22:05:57 +0100

    Update index_sessione.php

--- PHP/B_ES04 - Gestione delle sessioni nelle applicazioni web/index_sessione.php
+++ PHP/B_ES04 - Gestione delle sessioni nelle applicazioni web/index_sessione.php
@@ -1,3 +1,5 @@
+<!-- Alberto Angelo Quagliata -->
+
 <html>
 <head>
 		<title>Home page</title>
@@ -6,7 +8,7 @@
 <h1>Home page</h1><hr/>
 <a href="Login.php"><h2>Esegui Login</a><br/></h2>
 <a href="PaginaRis.php"><h2>Vai alla pagina riservata</a><br/></h2>
-<a href="Logout.php"><h2>Esegui logout</a><br/></h2>
+
 
 <hr/>
 </body>
