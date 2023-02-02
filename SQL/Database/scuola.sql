dfb46b3521eacf5276c2cd6b3304b5999cf5dce4
QuagliataAlberto <113895221+QuagliataAlberto@users.noreply.github.com>
2023-02-02 11:12:55 +0100

    Update scuola.sql

--- SQL/Database/scuola.sql
+++ SQL/Database/scuola.sql
@@ -1,1 +1,12 @@
+__CREAZIONE tabella corsi__
+
+CREATE TABLE corsi(
+    classe INT,
+    sezione CHAR(4),
+    nome_corso CHAR(25),
+    PRIMARY KEY(classe, sezione)
+    );
+    
+    
+__CREAZIONE tabella studenti__
 
