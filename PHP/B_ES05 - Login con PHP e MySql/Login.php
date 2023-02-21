5d7fa7fea1bb0535542e92c499de0b1f7fea1312
QuagliataAlberto <113895221+QuagliataAlberto@users.noreply.github.com>
2023-02-22 00:57:06 +0100

    Create Login.php

---
+++ PHP/B_ES05 - Login con PHP e MySql/Login.php
@@ -0,0 +1,56 @@
+<!-- Alberto Angelo Quagliata -->
+
+<?php
+session_start(); // Avvia la sessione php.
+require 'function.php';
+?>
+
+<html>
+	
+	<head>
+		<title>Login</title>
+	</head>
+	
+	<body> 
+		<?php
+			//definizione del db
+			define('DB_SERVER', 'localhost');
+			define('DB_NAME', 'gestione_utenti');
+			define('DB_USER', 'root');
+			define('DB_PASSWORD', '');
+
+			if(!isset($_SESSION['login']))
+			{
+				//il login non è ancora verificato
+				if(!isset($_POST['Login']))
+				{
+					//Visualizzo il form
+		?>
+					<form name="frmLogin" action="Login.php" method="POST">
+						<label>Username <input type="text" name="username" placeholder="username"></label><br />
+						<label>Password <input type="password" name="password" placeholder="password"></label><br />
+						<input type="submit" name="Login" />
+					</form>
+					<p>Non sei registrato? <a href="Registrazione.php"> Crea un account</a>.</p>
+					<p>Password dimenticata? <a href="PasswordChange.php"> Cambia Password</a>.</p><br/>
+		<?php
+					
+				}else
+					{
+						//connessione al database
+						login();
+					}
+			}
+			else
+			{
+				//L'utente ha già fatto il login
+				echo "L'utente ha già effetuato il login <br>";
+				echo "<a href='homepage.php'>Home</a><br>";
+				echo "<a href='Logout.php'>Esci</a><br>";
+			}
+		
+		?>
+		
+	</body> 
+	
+</html>
