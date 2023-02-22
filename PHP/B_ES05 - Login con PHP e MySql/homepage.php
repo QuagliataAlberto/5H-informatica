84178fd4a3f06ac8d1132c6eb8f872f4f0b9b6bd
QuagliataAlberto <113895221+QuagliataAlberto@users.noreply.github.com>
2023-02-22 01:06:14 +0100

    Create homepage.php

---
+++ PHP/B_ES05 - Login con PHP e MySql/homepage.php
@@ -0,0 +1,18 @@
+<!-- Alberto Angelo Quagliata -->
+
+<html>
+
+	<head>
+		<title>Home Page</title>
+	</head>
+	
+	<body>
+	
+		<h1>Home page</h1><hr/>
+		<a href="Login.php"><h2>Esegui Login</a><br/></h2>
+		<a href="PaginaRis.php"><h2>Vai alla pagina riservata</a><br/></h2>
+		<hr/>
+			
+	</body>
+	
+</html>
