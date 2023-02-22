76fbda63050961b885fbef103d6b972ca767cb7b
QuagliataAlberto <113895221+QuagliataAlberto@users.noreply.github.com>
2023-02-22 01:27:31 +0100

    Update homepage.php

--- PHP/B_ES05 - Login con PHP e MySql/homepage.php
+++ PHP/B_ES05 - Login con PHP e MySql/homepage.php
@@ -7,7 +7,7 @@
 	</head>
 	
 	<body>
-	
+		<h4>Benvenuto!</h4><hr/>
 		<h1>Home page</h1><hr/>
 		<a href="Login.php"><h2>Esegui Login</a><br/></h2>
 		<a href="PaginaRis.php"><h2>Vai alla pagina riservata</a><br/></h2>
