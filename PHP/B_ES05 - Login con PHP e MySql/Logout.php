3e06f5b3d091f32f35bd6fb73d754964d5c78c55
QuagliataAlberto <113895221+QuagliataAlberto@users.noreply.github.com>
2023-02-22 01:03:13 +0100

    Create Logout.php

---
+++ PHP/B_ES05 - Login con PHP e MySql/Logout.php
@@ -0,0 +1,25 @@
+<!-- Alberto Angelo Quagliata -->
+
+
+<html>
+	<head>
+		<title>Uscita - ITCS Erasmo da Rotterdam</title>
+	</head>
+
+	<body>
+
+		<?php
+
+			session_start();
+			session_destroy();
+			echo"<h3>Logout avvenuto</h3>";
+
+
+		?>
+
+			<a href="Login.php"><h2>Accedi di nuovo</h2></a>
+			<a href="homepage.php"><h2>Torna alla Home</h2></a>
+
+	</body>
+	
+</html>
